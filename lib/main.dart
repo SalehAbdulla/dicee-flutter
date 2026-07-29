668bd96522ea46a471e17fc260502dfb52dad1ca
salehabdulla <salehabdullaict@gmail.com>
2026-07-29 19:28:47 +0300

    Refactor iOS project settings and update main.dart layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,39 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
+  return runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(title: Text('Dicee'), backgroundColor: Colors.red),
         body: DicePage(),
       ),
     ),
-  );
+  ));
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
-
-  void rollDice() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: GestureDetector(
-              onTap: () {
-                rollDice();
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Image(image: AssetImage('images/dice1.png')),
           ),
-          Expanded(
-            child: GestureDetector(
-              onTap: () {
-                rollDice();
-              },
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-            ),
+        ),
+        SizedBox(width: 12),
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Image(image: AssetImage('images/dice2.png')),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
